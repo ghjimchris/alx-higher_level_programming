a6aedbf6f804722839aaabf292ee6853183ba465
ghjimchris <jimrajchris@gmail.com>
2023-03-28 03:58:55 -0700

    Advance task 2

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as err:
+        result = None
+        sys.stderr.write("Exception: {}\n".format(err))
+    return result
