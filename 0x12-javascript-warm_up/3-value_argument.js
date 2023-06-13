bcde65b33258f2963a53272b931f587546a6c721
ghjimchris <jimrajchris@gmail.com>
2023-06-13 09:46:39 -0700

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-	console.log('No argument);
-}	else{
-	console.log(process.argv[2])
+
+const argv = process.argv;
+if (argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
 }
