2ea9737bf60cf7ca5a7eb22877e9d67ac9512e4a
ghjimchris <jimrajchris@gmail.com>
2023-07-27 05:21:08 +0000

    Task 6 updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,15 @@
 #!/usr/bin/node
 
-/* computes the number of tasks completed by user id.
-    - the first argument is the API URL.
-    - only prints users with completed task.
-*/
 const request = require('request');
-const url = process.argv[2];
-request(url, function (error, response, body) {
-  if (error) console.log(error);
-  else if (response.statusCode === 200) {
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
     const completed = {};
-    JSON.parse(body).forEach(task => {
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
     });
     console.log(completed);
